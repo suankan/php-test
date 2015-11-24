9ff3eaf946030d1289a9049760883b8e0d9a18b9
Suan Kan <kansuan@gmail.com>
2015-11-24 14:27:29 +1100

    test

--- user_upload.php
+++ user_upload.php
@@ -1,1 +1,27 @@
+<?php
+/*
+Create a PHP script that is executed form the command line that accepts a CSV file as input (see command line directives below) and processes the CSV file. The parsed file data is to be inserted into a MySQL database. A CSV file will be provided as part of this task that contains test data, the script must be able to process this file.
+The PHP script will need to correctly handle the following criteria:
+• CSV file will contain user data and have three columns: name, surname, email (see table definition below)
+• CSV file will have an arbitrary list of users
+• Script will iterate through the CSV rows and insert each record into a dedicated
+MySQL database into the table “users”
+• The users database table will need to be created/rebuilt as part of the PHP script. This will be defined as a Command Line directive below.
+• Name and surname field should be set to be capitalised e.g. from “john” to “John” before being inserted into DB
+• Emails need to be set to be lower case before being inserted into DB
+• The script should validate the email address before inserting to make sure that it is valid (valid means that it is a legal email format e.g. “xxxx@asdf@asdf is not a legal format). In the instance that an email is invalid, no insert should be made to database and error message reported to STDOUT.
+We are looking for a script that is robust and gracefully handles errors/exceptions.
 
+Command line options:
+ --file [csv file name] – this is the name of the CSV to be parsed
+ --create_table – this will cause the MySQL users table to be built (and no further
+ action will be taken)
+ --dry_run – this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered.
+ -u – MySQL username
+ -p – MySQL password
+ -h – MySQL host
+ --help – which will output the above list of directives with details.
+*/
+
+
+?>
